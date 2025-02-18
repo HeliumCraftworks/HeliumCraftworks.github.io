54c954a74db1ba84ee37fb265b51264cfbc4a7a8
MichaelHealyM <michaelhealym@gmail.com>
2025-02-18 15:43:15 +0000

    Updated

--- static/script.js
+++ static/script.js
@@ -7,26 +7,27 @@ form.addEventListener('submit', function (event) {
     event.preventDefault();
     const formData = new FormData(form);
 
-    fetch('https://formspree.io/f/mbldkveb', {
-        method: 'POST',
-        body: formData
+    fetch(form.action, {
+        method: form.method,
+        body: formData,
+        headers: { 'Accept': 'application/json' }
     })
         .then(response => {
             if (response.ok) {
                 overlay.style.display = 'flex';
-                overlayContent.style.backgroundColor = 'green';
                 overlayMessage.textContent = 'Thank you for reaching out.'
                 form.reset();
             } else {
                 overlay.style.display = 'flex';
-                overlayContent.style.backgroundColor = 'red';
                 overlayMessage.textContent = 'There was an error with your submission. Please try again later.';
             }
         })
         .catch(error => {
             console.error('Error:', error);
             overlay.style.display = 'flex';
-            overlayContent.style.backgroundColor = 'red';
             overlayMessage.textContent = 'There was an error with your submission. Please try again later.';
         });
+    setTimeout(function () {
+        overlay.style.display = 'none';
+    }, 2000);
 });
