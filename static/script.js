41dae156687867c6ee143a17ace16b1883b276c3
MichaelHealyM <michaelhealym@gmail.com>
2025-02-18 15:32:22 +0000

    Updates

--- static/script.js
+++ static/script.js
@@ -1,15 +1,32 @@
-const form = document.getElementById('contactForm');
-const overlay = document.querySelector('.overlay');
-
-overlay.style.display = 'none';
+const form = document.getElementById('contact-form');
+const overlay = document.getElementById('overlay');
+const overlayContent = document.getElementById('overlay-content');
+const overlayMessage = document.getElementById('overlay-message');
 
 form.addEventListener('submit', function (event) {
     event.preventDefault();
-    setTimeout(function () {
-        overlay.style.display = 'flex';
-        form.reset();
-        setTimeout(function () {
-            overlay.style.display = 'none';
-        }, 3000);
-    }, 500);
+    const formData = new FormData(form);
+
+    fetch('https://formspree.io/f/mbldkveb', {
+        method: 'POST',
+        body: formData
+    })
+        .then(response => {
+            if (response.ok) {
+                overlay.style.display = 'flex';
+                overlayContent.style.backgroundColor = 'green';
+                overlayMessage.textContent = 'Thank you for reaching out.'
+                form.reset();
+            } else {
+                overlay.style.display = 'flex';
+                overlayContent.style.backgroundColor = 'red';
+                overlayMessage.textContent = 'There was an error with your submission. Please try again later.';
+            }
+        })
+        .catch(error => {
+            console.error('Error:', error);
+            overlay.style.display = 'flex';
+            overlayContent.style.backgroundColor = 'red';
+            overlayMessage.textContent = 'There was an error with your submission. Please try again later.';
+        });
 });
